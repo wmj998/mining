06915ba3de82deec4a80eb769cb48f0ff3bfad86
wmj998 <wmj998@foxmail.com>
2021-03-26 20:03:17 +0800

    Create 3-6.py

---
+++ chapter3/code/3-6.py
@@ -0,0 +1,14 @@
+# 代码3-6 餐饮销量数据统计量分析
+
+import pandas as pd
+
+catering_sale = 'D:/文件丶/数据分析与挖掘实战/chapter3/data/catering_sale.xls'  # 餐饮数据
+data = pd.read_excel(catering_sale, index_col = '日期')  # 读取数据，指定“日期”列为索引列
+data = data[(data['销量'] > 400)&(data['销量'] < 5000)]  # 过滤异常数据
+statistics = data.describe()  # 保存基本统计量
+
+statistics.loc['range'] = statistics.loc['max']-statistics.loc['min']  # 极差
+statistics.loc['var'] = statistics.loc['std']/statistics.loc['mean']  # 变异系数
+statistics.loc['dis'] = statistics.loc['75%']-statistics.loc['25%']  # 四分位数间距
+
+print(statistics)
