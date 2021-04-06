eadb85e0f8b0e9cecdfcc825c50e4e0dfea0fdb0
wmj998 <wmj998@foxmail.com>
2021-04-06 13:12:31 +0800

    Update code

---
+++ chapter4数据预处理/code/4-1.py
@@ -0,0 +1,23 @@
+import pandas as pd  # 导入数据分析库Pandas
+from scipy.interpolate import lagrange  # 导入拉格朗日插值函数
+
+inputfile = 'D:/文件丶/数据分析与挖掘实战/chapter4数据预处理/demo/data/catering_sale.xls'  # 销量数据路径
+outputfile = 'D:/文件丶/数据分析与挖掘实战/chapter4数据预处理/demo/sales.xls'  # 输出数据路径
+
+data = pd.read_excel(inputfile)  # 读入数据
+data['销量'][(data['销量'] < 400) | (data['销量'] > 5000)] = None  # 过滤异常值，将其变为空值
+# 自定义列向量插值函数
+# s为列向量，n为被插值的位置，k为取前后的数据个数，默认为5
+def ployinterp_column(s, n, k=5):
+  y = s.index[list(range(n-k, n)) + list(range(n+1, n+1+k))]  # 取数
+  y = y[y.notnull()]  # 剔除空值
+  # return lagrange(y, list(y))(n)  # 插值并返回插值结果
+  return lagrange([x + 1 for x in range(len(y))], list(y))(len(y) * 0.618)  # 插值并返回插值结果
+
+# 逐个元素判断是否需要插值
+for i in data.columns:
+  for j in range(len(data)):
+    if (data[i].isnull())[j]:  # 如果为空即插值。
+      data[i][j] = ployinterp_column(data[i], j)
+
+data.to_excel(outputfile)  # 输出结果，写入文件
