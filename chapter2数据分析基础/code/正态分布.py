042fe6bd785db934f3ccf927ab4c1bd6afbcb05b
wmj998 <wmj998@foxmail.com>
2021-04-09 13:16:27 +0800

    Update code

---
+++ chapter2数据分析基础/code/正态分布.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+n = np.random.randn(1000)   #生成均值为0，标准差为0.1的一维正态分布样本1000个
+sns.distplot(n)                      #直方图
+plt.show()# 显示
