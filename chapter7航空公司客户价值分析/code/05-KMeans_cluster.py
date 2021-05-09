635d8ab78a85718af74e6694bafa3efef2d4d1d6
wmj998 <wmj998@foxmail.com>
2021-05-09 15:18:47 +0800

    Update code

--- chapter7航空公司客户价值分析/code/05-KMeans_cluster.py
+++ chapter7航空公司客户价值分析/code/05-KMeans_cluster.py
@@ -32,21 +32,21 @@ cluster_center = pd.DataFrame(kmeans_model.cluster_centers_,
 # 代码7-10
 
 # %matplotlib inline
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
 # 客户分群雷达图
 labels = ['ZL', 'ZR', 'ZF', 'ZM', 'ZC']
 legen = ['客户群' + str(i + 1) for i in cluster_center.index]  # 客户群命名，作为雷达图的图例
 lstype = ['-', '--', (0, (3, 5, 1, 5, 1, 5)), ':', '-.']
-kinds = list(cluster_center.iloc[:, 0])
-# 由于雷达图要保证数据闭合，因此再添加L列，并转换为 np.ndarray
-cluster_center = pd.concat([cluster_center, cluster_center[['ZL']]], axis=1)
-centers = np.array(cluster_center.iloc[:, 0:])
+
+# 由于雷达图要保证数据闭合，因此再添加ZL列，并转换为 np.ndarray
+centers = pd.concat([cluster_center, cluster_center[['ZL']]], axis=1).values
 
 # 分割圆周长，并让其闭合
-n = len(labels)
+n = len(legen)
 angle = np.linspace(0, 2 * np.pi, n, endpoint=False)
-angle = np.concatenate((angle, [angle[0]]))
-labels = np.concatenate((labels, [labels[0]]))
+
+angles = np.concatenate([angle, [angle[0]]])
 
 # 绘图
 fig = plt.figure(figsize=(8, 6))
@@ -54,8 +54,8 @@ ax = fig.add_subplot(111, polar=True)  # 以极坐标的形式绘制图形
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号 
 # 画线
-for i in range(len(kinds)):
-    ax.plot(angle, centers[i], linestyle=lstype[i], linewidth=2, label=kinds[i])
+for i in range(n):
+    ax.plot(angles, centers[i], linestyle=lstype[i], linewidth=2)
 # 添加属性标签
 ax.set_thetagrids(angle * 180 / np.pi, labels)
 plt.title('客户特征分析雷达图')
