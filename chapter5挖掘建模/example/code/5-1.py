8820b625bd354a187dc61ac9e833396d7a10dacc
wmj998 <wmj998@foxmail.com>
2021-05-10 15:08:01 +0800

    Update code

---
+++ chapter5挖掘建模/example/code/5-1.py
@@ -0,0 +1,14 @@
+# logistic_regression 逻辑回归
+
+import pandas as pd
+from sklearn.linear_model import LogisticRegression as LR
+
+# 参数初始化
+filename = '../data/bankloan.xls'
+data = pd.read_excel(filename)
+x = data.iloc[:,:8].values
+y = data.iloc[:,8].values
+
+lr = LR()  # 建立逻辑回归模型
+lr.fit(x, y)  # 用筛选后的特征数据来训练模型
+print('模型的平均准确度为：%s' % lr.score(x, y))
