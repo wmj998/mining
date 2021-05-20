d544a511aebeb138fb20d344a79e4a731ca4cf1d
wmj998 <wmj998@foxmail.com>
2021-05-20 15:26:20 +0800

    Update code

--- chapter2数据分析基础/code/2、pandas.py
+++ chapter2数据分析基础/code/2、pandas.py
@@ -1,42 +1,41 @@
 import pandas as pd
 
-ob = pd.Series([2,3,5,7,3,1],index=['a','b','c','d','e','f'])
+ob = pd.Series([2, 3, 5, 7, 3, 1], index=['a', 'b', 'c', 'd', 'e', 'f'])
 print(ob)
 
-dt = [1,2,3,4,5]
-id = ['a','b','c','d','e']
-print(pd.Series(dt,id))
-
+dt = [1, 2, 3, 4, 5]
+id = ['a', 'b', 'c', 'd', 'e']
+print(pd.Series(dt, id))
 
 data_1 = {
-        'subject_id': ['1', '2', '3', '4', '5'],
-        'first_name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
-        'last_name': ['Anderson', 'Ackerman', 'Ali', 'Aoni', 'Atiches']}
+    'subject_id': ['1', '2', '3', '4', '5'],
+    'first_name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+    'last_name': ['Anderson', 'Ackerman', 'Ali', 'Aoni', 'Atiches']}
 data_2 = {
-        'subject_id': ['4', '5', '6', '7', '8'],
-        'first_name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
-        'last_name': ['Bonder', 'Black', 'Balwner', 'Brice', 'Btisan']}
+    'subject_id': ['4', '5', '6', '7', '8'],
+    'first_name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+    'last_name': ['Bonder', 'Black', 'Balwner', 'Brice', 'Btisan']}
 data_3 = {
-        'subject_id': ['1', '2', '3', '4', '5', '7', '8', '9', '10', '11'],
-        'test_id': [51, 15, 15, 61, 16, 14, 15, 1, 61, 16]}
+    'subject_id': ['1', '2', '3', '4', '5', '7', '8', '9', '10', '11'],
+    'test_id': [51, 15, 15, 61, 16, 14, 15, 1, 61, 16]}
 df1 = pd.DataFrame(data_1)
 df2 = pd.DataFrame(data_2)
 df3 = pd.DataFrame(data_3)
 
-df12x = pd.concat([df1,df2],axis=1)
-df12y = pd.concat([df1,df2],axis=0)
+df12x = pd.concat([df1, df2], axis=1)
+df12y = pd.concat([df1, df2], axis=0)
 
 print(df1[:3])
 
-data = pd.DataFrame([['Neijiang',20,6],['Chengdou',21,8],['Beijing',24,10]],columns=['Cite','year','cash'])
+data = pd.DataFrame([['Neijiang', 20, 6], ['Chengdou', 21, 8], ['Beijing', 24, 10]], columns=['Cite', 'year', 'cash'])
 
-data['book'] = ['001','002','003']
+data['book'] = ['001', '002', '003']
 
 print(data['year'])
 
 print(data.drop(columns='book'))
 
-print((data['year'])**(1/2))
-print((data['year'])**2)
-print((data['cash'])**(1/2))
-print((data['cash'])**2)
+print((data['year']) ** (1 / 2))
+print((data['year']) ** 2)
+print((data['cash']) ** (1 / 2))
+print((data['cash']) ** 2)
--- chapter2数据分析基础/code/3、matplotlib.py
+++ chapter2数据分析基础/code/3、matplotlib.py
@@ -1,33 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.rcParams['font.sans-serif'] = ['SimHei'] #解决plt中文显示的问题
-plt.rcParams['axes.unicode_minus'] = False #解决plt负号显示的问题
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 解决plt中文显示的问题
+plt.rcParams['axes.unicode_minus'] = False  # 解决plt负号显示的问题
 
-x = np.linspace(0,2)
-y = (np.sin(x-2))**2*(np.exp(-x**2))
+x = np.linspace(0, 2)
+y = (np.sin(x - 2)) ** 2 * (np.exp(-x ** 2))
 
-plt.title(r"$y = sin(x-2)^2e^{-x^2}$")
+plt.title("$y = sin(x-2)^2e^{-x^2}$")
 plt.xlabel("x")
 plt.ylabel("y")
-plt.plot(x,y)
+plt.plot(x, y)
 plt.show()
 
-
-x = np.linspace(0,6)
+x = np.linspace(0, 6)
 y = np.cos(x)
 plt.title("Example")
 plt.xlabel("x")
 plt.ylabel("y")
-plt.plot(x,y,label='cos(x)',color='g')
+plt.plot(x, y, label='cos(x)', color='g')
 plt.legend()
 plt.show()
 
-
-
-x = [0.15,0.3,0.45,0.1]
-explode = [0,0,0,0]
-labels = ['apples','oranges','bananas','pears']
-colors = ['r','y','g','b']
-plt.pie(x,explode,labels,colors,autopct='%.1f%%')
+x = [0.15, 0.3, 0.45, 0.1]
+explode = [0, 0, 0, 0]
+labels = ['apples', 'oranges', 'bananas', 'pears']
+colors = ['r', 'y', 'g', 'b']
+plt.pie(x, explode, labels, colors, autopct='%.1f%%')
 plt.show()
